bb816526fc363fc2ab6baa217314e081e16855e1
sayalipatil86-prog <sayalip547@gmail.com>
2026-09-15 12:33:34 +0530

    Add NexGravision company name next to logo image

--- src/components/BrandLogo.jsx
+++ src/components/BrandLogo.jsx
@@ -2,15 +2,31 @@ import React from "react";
 
 export default function BrandLogo({ size = 38 }) {
   return (
-    <img
-      src="/LOGOIMG.png"
-      alt="NexGravision"
-      className="brand-logo-img"
-      style={{
-        height: size,
-        width: "auto",
-        objectFit: "contain",
-      }}
-    />
+    <div style={{ display: "flex", alignItems: "center", gap: 10 }}>
+      <img
+        src="/LOGOIMG.png"
+        alt="NexGravision"
+        className="brand-logo-img"
+        style={{
+          height: size,
+          width: "auto",
+          objectFit: "contain",
+        }}
+      />
+      <div>
+        <div
+          style={{
+            fontFamily: "'Space Grotesk', sans-serif",
+            fontSize: size * 0.55,
+            fontWeight: 700,
+            letterSpacing: "0.04em",
+            color: "#FFFFFF",
+            lineHeight: 1.1,
+          }}
+        >
+          NexGravision
+        </div>
+      </div>
+    </div>
   );
 }
